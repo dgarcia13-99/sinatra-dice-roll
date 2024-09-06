0f258b6f6fa19c71c1e2cdfe354bfa6da7a40c26
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-06 20:54:28 +0000

    fixed dice rolls paths to function like the 100_6 dice roll path

--- dice.rb
+++ dice.rb
@@ -10,20 +10,8 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
 get("/") do
-  "<h1>Dice Roll</h1>
-  <body>
-    <div>
-      <ul>
-        <li><a href=\"https://sturdy-eureka-q79pxgggxx9rc4vqj-4567.app.github.dev/dice/2/6\"> Roll two six-sided dice</a></li>
-        <li><a href=\"https://sturdy-eureka-q79pxgggxx9rc4vqj-4567.app.github.dev/dice/2/10\"> Roll two ten-sided dice</a></li>
-        <li><a href=\"https://sturdy-eureka-q79pxgggxx9rc4vqj-4567.app.github.dev/dice/1/20\"> Roll one twenty-sided dice</a></li>
-        <li><a href=\"https://sturdy-eureka-q79pxgggxx9rc4vqj-4567.app.github.dev/dice/5/4\"> Roll five four-sided dice</a></li>
-      </ul>
-    </div>
-  </body>"
+  erb(:elephant)
 end
-
-
 
 get("/zebra") do
   "We must add a route for each path we want to support"
@@ -38,45 +26,59 @@ get("/koala") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls=[]
+
+  2.times do
+  die = rand(1..6)
+  @rolls.push(die)
+  sum += die
+end
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  #@outcome_2_6 = "You rolled a #{@rolls[0]} and a #{@rolls[1]} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get ("/dice/2/10") do
-  first=rand(1..10)
-  second=rand(1..10)
-  sum=first+second
+  @rolls=[]
 
-  answer="You rolled a #{first} and a #{second}, resulting in a total sum of #{sum}."
+  2.times do
+    die=rand(1..10)
+    @rolls.push(die)
+  end
 
-  "<h1>2d10</h1>
-  <p>#{answer}</p>"
+  #@outcome_2_10="You rolled a #{first} and a #{second}, resulting in a total sum of #{sum}."
+
+  erb(:two_ten)
 end
 
 get ("/dice/1/20") do
-  dice=rand(1..20)
-  final_answer="You rolled a #{dice}."
+  @dice=rand(1..20)
+  #@outcome_1_20="You rolled a #{@dice}."
 
-  "<h1>1d20</h1>
-  <p>#{final_answer}</p>"
+  erb(:one_twenty)
 end
 
 get ("/dice/5/4") do
-  first=rand(1..4)
-  second=rand(1..4)
-  third=rand(1..4)
-  fourth=rand(1..4)
-  fifth=rand(1..4)
-  sum=first+second+third+fourth+fifth
+  @rolls=[]
 
-  outcome="You rolled a #{first}, a #{second}, a #{third}, a #{fourth}, and a #{fifth}, resulting in a total sum of #{sum}."
+  5.times do 
+    die=rand(1..4)
+    @rolls.push(die)
+  end
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  #@outcome_5_4="You rolled a #{first}, a #{second}, a #{third}, a #{fourth}, and a #{fifth}, resulting in a total sum of #{sum}."
+
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls=[]
+  100.times do
+    die= rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
+
 end
